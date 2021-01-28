abadde1dbc56f5f91798e2d2f6c2a25b4d4c9780
ptsteadman <ptsteadman@gmail.com>
2021-01-28 20:28:45 +0000

    Deploying to gh-pages from  @ 05ae3cd65ded036dd7e96b79568934b18e783da8 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7fd068982253f5cbc52af7efc385170c.js"
+  "/precache-manifest.2fe9c5c477f21f94405bbe812b246a85.js"
 );
 
 self.addEventListener('message', (event) => {
